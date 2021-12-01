1a51368350551f9fbd70b2bb0b26d1f1cdca81d1
wilax550 <wilax@telia.com>
2021-12-01 11:37:05 +0100

    update

--- recommender.py
+++ recommender.py
@@ -37,7 +37,8 @@ def find_similarity(column, user, number,scores, city): # city == staden man kom
         value.append(score) # sparar värdet i value
         # ÄNDRA HÄR FÖR ATT FÅ FLERA STÄDER
     similarity = pd.Series(value, index=new_df.index)
-    city_similar = similarity.sort_values(ascending=False).astype(float).iloc[:5] #Instead of idxmax use 5 top values for multiple cities?
+    city_similar = similarity.sort_values(ascending=False).astype(float).iloc[1:5] #Instead of idxmax use 5 top values for multiple cities?
+    
     # message = f'Based on your aggregate preferences and ratings, {city_similar} is the top recommended city to move/travel to.'
     return city_similar
 
@@ -85,30 +86,34 @@ def main():
     city = st.selectbox("Location of Residence", location)
     preference = st.multiselect("Choose the 5 features that matters to you the most in a city",scores.columns)
     if st.checkbox("Rate the features"):
-         if len(preference) == 5 :
+         if len(preference) == 1 :
             level1 = st.slider(preference[0], 1,10)
-            level2 = st.slider(preference[1], 1,10)
-            level3 = st.slider(preference[2], 1,10)
-            level4 = st.slider(preference[3], 1,10)
-            level5 = st.slider(preference[4], 1,10)
+          #  level2 = st.slider(preference[1], 1,10)
+          #  level3 = st.slider(preference[2], 1,10)
+          #  level4 = st.slider(preference[3], 1,10)
+           # level5 = st.slider(preference[4], 1,10)
             if st.button("Recommend", key="hi"):
-                user = np.array([level1, level2,level3,level4,level5])
+                user = np.array([level1]) # level2,level3,level4,level5])
                 column = preference # hämtar kolumn siffror för varje preferens som användaren fyllt i
                 number = len(preference)
                 city_similar = find_similarity(column, user, number,scores,city)
-                city_similar.to_csv('newTextFile.csv', sep='\t')
+                col_list = ["City"]
+                city_similar.to_csv('newTextFile.csv')
+                df_cities = pd.read_csv('newTextFile.csv', usecols=col_list)
+                df_cities["City"].to_csv('nameOfCities.csv')
                 with st.spinner("Analysing..."):
                     time.sleep(5)
                 st.text(f'\n\n\n')
                 st.markdown('--------------------------------------------**Recommendation**--------------------------------------------')
                 st.text(f'\n\n\n\n\n\n')
-                for index, val in enumerate(city_similar):
-                    st.markdown(f'Based on your aggregate preferences and ratings, **{index}** is the top recommended city to move/travel to.')
-                    title, country , subtitle, response = final_answer(df, index, data)
+                for index, val in enumerate(df_cities["City"]):
+                    st.markdown(f'Based on your aggregate preferences and ratings, **{df_cities["City"][1]}** is the top recommended city to move/travel to.')
+                    title, country , subtitle, response = final_answer(df, df_cities["City"][1], data)
                     st.text(f'\n\n\n\n\n\n')
                     
+                    
                     st.markdown(f'----------------------------------------------**{title}**---------------------------------------------')
-                    st.write(f'{index} is a city in {country}. {response}')
+                    st.write(f'{df_cities["City"][1]} is a city in {country}. {response}')
                     st.text(subtitle)
                     #st.table(breakdown.style.format({'Score':'{:17,.1f}'}).background_gradient(cmap='Blues').set_properties(subset=['Score'], **{'width': '250px'}))
                     st.markdown(f'For more info on city rank scores, check [here](https://www.nestpick.com/millennial-city-ranking-2018/)')
